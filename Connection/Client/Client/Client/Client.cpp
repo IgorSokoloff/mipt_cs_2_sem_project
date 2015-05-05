8b14edfff7b58bcf85ef4770f3911d13285e9f13
KaloshinPE <kaloshinpavel@gmail.com>
2015-05-05 22:06:51 +0400

    Added working client part.
    Now it can try to connect to server when started and wait for server's
    answer. When the connection is established, client waits for commands
    from server. We can ask it to send us something, to finish itself and to
    close connection and start listening. The last function isn't working
    yet.

--- Connection/Client/Client/Client/Client.cpp
+++ Connection/Client/Client/Client/Client.cpp
@@ -1,3 +1,6 @@
+// VirusConnectionSystem.cpp: определ€ет точку входа дл€ консольного приложени€.
+//
+
 #include "stdafx.h"
 #include < winsock2.h > 
 #include < ws2tcpip.h >
@@ -8,19 +11,20 @@
 #define DEFAULT_SERVER_PORT 27015
 #define DEFAULT_BUFLEN 512
 #define	DEFAULT_SERVER_IP "192.168.137.1"
+#define bufsize 512
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::cout << "Start work, motherfucker!" << std::endl;
-
+	std::cout << "Start work, motherfucker!!!" << std::endl;
+	char OUR_DEFAULT_PORT_CHAR[] = "27015";  //сюда стоит прикрутить проверку, не зан€т ли стандартный порт. Ќо пока тупо используем его.
 	//»нициализируем WSA
 	WSADATA wsaData;
 	int result;
 	result = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (result != 0)
 	{
-		std::cout << "Mistake in function WSAStartup: " << result << std::endl;
+		std::cout << "Error in function WSAStartup: " << result << std::endl;
 		getchar();
 		return 1;
 	}
@@ -30,7 +34,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (clientSocket == INVALID_SOCKET)
 	{
-		std::cout << "Mistake in function socket(): " << WSAGetLastError() << std::endl;
+		std::cout << "Error in function socket() while trying to create it first time: " << WSAGetLastError() << std::endl;
 		getchar();
 		WSACleanup();
 		return 1;
@@ -44,19 +48,151 @@ int _tmain(int argc, _TCHAR* argv[])
 	clientService.sin_port = htons(DEFAULT_SERVER_PORT);
 
 	std::cout << "Trying to connect to " << DEFAULT_SERVER_IP << std::endl;
-
-	result = connect(clientSocket, (reinterpret_cast< SOCKADDR* >(&clientService)), sizeof(clientService));
-	if (result != 0)
+	while (true)
 	{
-		std::cout << "Mistake in function connect(): " << WSAGetLastError() << std::endl;
-		getchar();
-		WSACleanup();
-		return 1;
+		result = connect(clientSocket, (reinterpret_cast<SOCKADDR*>(&clientService)), sizeof(clientService));
+		if (result == 0)
+			break;
 	}
+
+	/*if (result != 0)
+	{
+	std::cout << "Error in function connect() while trying to connect to server first time: " << WSAGetLastError() << std::endl;
+	getchar();
+	WSACleanup();
+	return 1;
+	}*/
 
 	std::cout << "Successful connection!" << std::endl;
 
-	//	char data[512];
+	//—лушаем, что нам пришлют. Ќабор комманд:
+	//0-закрываем соединение, переходим в режим прослушивани€;
+	//1-закрываем соединение, завершем работу;
+	//2-отсылаем смайл :)
+	while (true)
+	{
+		// получаем данные
+		char buf[bufsize];
+		char x = buf[bufsize];
+		int r = recv(clientSocket, buf, bufsize, 0);
+		if (r > 0)
+		{
+			//std::cout << "Got " << r << " bytes" << std::endl;
+			int i = 0;
+			std::cout << "Got Message: ";
+			while (buf[i] != x && i <= 512)
+			{
+				std::cout << buf[i];
+				i++;
+			}
+			std::cout << std::endl;
+		}
+		else if (r == 0)
+		{
+			std::cout << "Disconnect" << std::endl;
+			WSACleanup();
+			return 0;
+		}
+		else
+		{
+			std::cout << "Error in function recv(): " << WSAGetLastError() << std::endl;
+			WSACleanup();
+			return 1;
+		}
+
+		// ≈сли прислали 2 - отсылаем смайл
+		if (buf[0] == '2')
+		{
+			std::cout << "got 2, now I'll send a piece of happyness" << std::endl;
+			char data[] = ":)";
+			result = -1;
+			result = send(clientSocket, data, static_cast< int >(strlen(data)), 0);
+			if (result < 0)
+			{
+				std::cout << "Error in function send() while sanding information: " << WSAGetLastError() << std::endl;
+				return 1;
+			}
+			continue;
+		}
+		// ≈сли прислали 0 - отключаемс€ и начинаем слушать.
+		if (buf[0] == '0')
+		{
+			std::cout << "Got 0, now I'll start listenning..." << std::endl;
+
+			closesocket(clientSocket);
+
+			struct addrinfo *result = NULL, *ptr = NULL, hints;
+			ZeroMemory(&hints, sizeof(hints));
+			hints.ai_family = AF_INET;
+			hints.ai_socktype = SOCK_STREAM;
+			hints.ai_protocol = IPPROTO_TCP;
+			hints.ai_flags = AI_PASSIVE;
+
+
+
+			int iResult = getaddrinfo(NULL, OUR_DEFAULT_PORT_CHAR, &hints, &result);
+			if (iResult != 0)
+			{
+				std::cout << "Error at getaddrinfo while creating listenning socket: " << iResult << std::endl;
+				WSACleanup();
+				return 1;
+			}
+
+			SOCKET listenSocket = INVALID_SOCKET;
+			listenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+			if (listenSocket == INVALID_SOCKET)
+			{
+				std::cout << "Error at socket() while creating listenning socket: " << WSAGetLastError() << std::endl;
+				freeaddrinfo(result);
+				WSACleanup();
+				return 1;
+			}
+
+			iResult = bind(listenSocket, result->ai_addr, result->ai_addrlen);
+			if (iResult == SOCKET_ERROR)
+			{
+				std::cout << "Bind listenning socket failed with error: " << WSAGetLastError() << std::endl;
+				freeaddrinfo(result);
+				closesocket(listenSocket);
+				WSACleanup();
+				return 1;
+			}
+
+			std::cout << "Listening..." << std::endl;
+
+			if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+			{
+				std::cout << "Listen failed with error: " << WSAGetLastError() << std::endl;
+				closesocket(listenSocket);
+				WSACleanup();
+				return 1;
+			}
+
+			freeaddrinfo(result);
+
+
+			clientSocket = INVALID_SOCKET;
+
+			clientSocket = accept(listenSocket, NULL, NULL);
+			if (clientSocket == INVALID_SOCKET)
+			{
+				std::cout << "Accept failed with error: " << WSAGetLastError() << std::endl;
+				closesocket(listenSocket);
+				WSACleanup();
+				return 1;
+			}
+
+		}
+		if (buf[0] == '1')
+		{
+			std::cout << "Got 1, now I'll close meyself." << std::endl;
+			closesocket(clientSocket);
+			WSACleanup();
+			return 0;
+		}
+
+	}
 
 	return 0;
 }
+

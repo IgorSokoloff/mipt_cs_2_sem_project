91384d63a51ee9c1dbccfcb8f8f50e3c0a150b16
Mikhail Maslov <mkmaslov@gmail.com>
2015-05-09 13:59:57 +0300

    Redone formatting.
    Better comment reading experience.

--- Virus_Part/main.asm
+++ Virus_Part/main.asm
@@ -8,14 +8,18 @@
 
 ;-----------------------------------------------------------------------------------------
 ;Some options for compiler.
-.386 ;Processor directive. Shows what architecture we use.
-.model flat, stdcall ;Initialising memory models (memorymodel - flat, langtype - stdcall).
-option casemap:none ;Turning on symbol case sensitivity.
+;-----------------------------------------------------------------------------------------
+.386                                     ; Processor directive. Shows what architecture we 
+										 ; use.
+.model flat, stdcall                     ; Initialising memory models (memorymodel - flat, 
+										 ; langtype - stdcall).
+option casemap:none                      ; Turning on symbol case sensitivity.
 ;-----------------------------------------------------------------------------------------
 
 ;-----------------------------------------------------------------------------------------
-;Some really strange macros. It works, but for me it's like magic.
-;Solution: This macros is used for ExitProcess to function properly.
+; Some really strange macros. It works, but for me it's like magic.
+; Solution: This macros is used for ExitProcess to function properly.
+;-----------------------------------------------------------------------------------------
 pushz macro szText:VARARG
     local nexti
     call  nexti
@@ -24,22 +28,22 @@ nexti:
 endm
 ;-----------------------------------------------------------------------------------------
 ;Some pre-code includes.
-includelib C:\masm32\lib\kernel32.lib ;Including library for ExitProcess function.
 ;-----------------------------------------------------------------------------------------
-ExitProcess PROTO :DWORD ;We need this string that declares the prototype of ExitProcess 
-						 ;function (arguments that function will use).						
+includelib C:\masm32\lib\kernel32.lib    ; Including library for ExitProcess function.
 ;-----------------------------------------------------------------------------------------
-.data ;Beginning of data section.
-    db 0 ;Define byte -> 0.
+ExitProcess PROTO :DWORD                 ; We need this string that declares the prototype 
+						                 ; of ExitProcess function (arguments that 
+										 ; function will use).						
 ;-----------------------------------------------------------------------------------------
-.code ;Beginning of a code section.
-    invoke ExitProcess, 0 ;Exiting to Windows.
+.data                                    ;Beginning of data section.
+    db 0                                 ;Define byte -> 0. (Need this for ExitProcess)
 ;-----------------------------------------------------------------------------------------
-start: ;Declaring start link.
-
-include res\start_code.asm ;Including start_code.asm.
-include res\virus_code.asm ;Including virus_code.asm - main virus code.
-include res\data.asm ;Including data.asm
-
-end start ;End of code section by start link.
+.code                                    ;Beginning of a code section.
+    invoke ExitProcess, 0                ;Exiting to Windows.
+;-----------------------------------------------------------------------------------------
+start:                                   ;Declaring start link.
+include res\start_code.asm               ;Including start_code.asm.
+include res\virus_code.asm               ;Including virus_code.asm - main virus code.
+include res\data.asm                     ;Including data.asm
+end start                                ;End of code section by start link.
 ;-----------------------------------------------------------------------------------------
